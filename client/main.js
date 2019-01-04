5971b9676d440c965f70caa3a5fbe2ae006df0cb
Andrew Liebchen <andrewliebchen@gmail.com>
2019-01-04 09:20:49 -0800

    Add system diagram

--- client/main.js
+++ client/main.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
+import { Provider } from "rebass";
 
 import App from "../imports/ui/App.js";
 
 Meteor.startup(() => {
-  render(<App />, document.getElementById("render-target"));
+  render(
+    <Provider>
+      <App />
+    </Provider>,
+    document.getElementById("render-target")
+  );
 });
--- imports/ui/System.js
+++ imports/ui/System.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { Flex, Heading } from "rebass";
 
-const System = props => <div>{props.match.params.stub}</div>;
+const System = props => (
+  <Flex flexDirection="column">
+    <Heading>{props.match.params.stub}</Heading>
+    <img src="/images/systemDiagram.svg" width="100%" height="auto" />
+  </Flex>
+);
 
 export default withRouter(System);
