0257a58dc3ca0ee895fd005594511c414277035f
Andrew Liebchen <andrewliebchen@gmail.com>
2019-01-06 22:07:26 -0800

    Movement checker WIP

--- imports/ui/System.js
+++ imports/ui/System.js
@@ -5,10 +5,12 @@ import { withTracker } from "meteor/react-meteor-data";
 import PropTypes from "prop-types";
 import React from "react";
 import Page from "./Page";
+import { isInRange } from "../utils/helpers";
 
 const System = props => (
   <Page title={props.system.name}>
     <img src="/images/systemDiagram.svg" width="800px" height="auto" />
+    {console.log(isInRange([0, 4, 0], [1, 4, 3], 1))}
   </Page>
 );
 
--- imports/utils/helpers.js
+++ imports/utils/helpers.js
@@ -1,1 +1,18 @@
-export const isInRange = (start, end, range) =>
+export const isInRange = (start, end, range) => {
+  /*
+    Coordinates are an array [0, 4, 0]...ship is in quadrant 0, ring 4, sector 0.
+    Ships cannot move diagonally.
+    If the ship has movement of 1, it can move to:
+    * Deep Space: [0, 5, 0]
+    * Within its own ring: [0, 4, 1], [1, 4, 3]
+    * Down system: [0, 3, 0]
+  */
+
+  let rangeCount = range;
+
+  start.map((value, i) => (rangeCount -= Math.abs(value - end[i])));
+
+  // // TODO: Need to do something different if moving between quadrants
+
+  return rangeCount >= 0;
+};
